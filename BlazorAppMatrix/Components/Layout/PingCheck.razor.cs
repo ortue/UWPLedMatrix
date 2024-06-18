fa68b28558dc550bb916923765beddb0a79498d5
Benoit Vendette <bvendette@gmail.com>
2024-06-17 20:16:32 -0400

    è

--- BlazorAppMatrix/Components/Layout/PingCheck.razor.cs
+++ BlazorAppMatrix/Components/Layout/PingCheck.razor.cs
@@ -7,11 +7,23 @@ namespace BlazorAppMatrix.Components.Layout
 {
   public partial class PingCheck
   {
-    public List<long> Moyenne { get; set; } = [];
+    public List<long> Moyenne { get; set; } = [3];
     public long Max { get; set; } = 20;
     public int TimeOut { get; set; }
     public bool Inter { get; set; }
     public bool ShowTimeOut { get; set; }
+    public long Ping { get; set; } = 0;
+
+    public string PingStr
+    {
+      get
+      {
+        if (Ping == 0)
+          return "--";
+
+        return Ping.ToString();
+      }
+    }
 
     private void Set()
     {
@@ -32,39 +44,20 @@ namespace BlazorAppMatrix.Components.Layout
         if (cycle % 1000 == 0)
           ShowTimeOut = !ShowTimeOut;
 
-        if (!Inter && cycle % 100 == 0)
+        if (!Inter && cycle++ % 100 == 0)
         {
           pingSender.SendAsync("google.com", 10000);
 
           Inter = true;
         }
 
-        Background.Bleu(Pixels);
-        Pixels.SendPixels();
-        cycle++;
-      }
-    }
-
-    private void PingCompletedCallback(object sender, PingCompletedEventArgs e)
-    {
-      Pixels.Reset();
-
-      if (e.Reply is PingReply reply)
-      {
-        long ping = reply.RoundtripTime;
-
-        if (reply.Status == IPStatus.Success)
-          Moyenne.Add(ping);
-        else
-          TimeOut++;
-
         for (int i = 1; i <= 20; i++)
           if (Moyenne.Count > i)
           {
             long point = Moyenne[^i];
 
-            if (ping > Max)
-              Max = ping;
+            if (Ping > Max)
+              Max = Ping;
 
             long maxY = 20 * point / Max;
 
@@ -75,84 +68,38 @@ namespace BlazorAppMatrix.Components.Layout
               Pixels.Get(20 - i, 20 - y).SetColor(63 / i, 63 / i, 127 / i);
           }
 
-        int x = Moyenne.Average().ToString("0").Length * 4;
-
         Pixels.Set(CaractereList.Print("PING", 1, 1, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
 
         if (ShowTimeOut)
           Pixels.Set(CaractereList.Print((TimeOut / (double)Moyenne.Count * 100).ToString("0%"), 1, 7, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
         else
-          Pixels.Set(CaractereList.Print(Moyenne.Average().ToString("0"), 20 - x, 7, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
+          Pixels.Set(CaractereList.Print(Moyenne.Average().ToString("0"), 20 - Moyenne.Average().ToString("0").Length * 4, 7, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
 
-        x = ping.ToString().Length * 4;
+        Pixels.Set(CaractereList.Print(PingStr, 20 - PingStr.Length * 4, 13, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
 
-        if (reply.Status == IPStatus.Success)
-          Pixels.Set(CaractereList.Print(ping.ToString(), 20 - x, 13, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
-        else
-          Pixels.Set(CaractereList.Print("--", 14, 13, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
+        Background.Bleu(Pixels);
+        Pixels.SendPixels();
+        Pixels.Reset();
       }
+    }
 
-      using ManualResetEventSlim waitHandle = new(false);
-      waitHandle.Wait(TimeSpan.FromMilliseconds(100));
+    private void PingCompletedCallback(object sender, PingCompletedEventArgs e)
+    {
+      if (e.Reply is PingReply reply)
+      {
+        Ping = reply.RoundtripTime;
+        Moyenne.Add(Ping);
+
+        if (reply.Status != IPStatus.Success)
+          TimeOut++;
+      }
+      else
+      {
+        Ping = 0;
+        TimeOut++;
+      }
 
       Inter = false;
     }
-
-    //private double SetPing(double cycle)
-    //{
-    //  Ping pingSender = new();
-    //  bool showTimeOut = false;
-
-    //  if (cycle % 1000 < 500)
-    //    showTimeOut = !showTimeOut;
-
-    //  if (cycle++ % 100 == 0)
-    //  {
-    //    Pixels.Reset();
-
-    //    PingReply reply = pingSender.Send("google.com", 10000);
-    //    long ping = reply.RoundtripTime;
-
-    //    if (reply.Status == IPStatus.Success)
-    //      Moyenne.Add(ping);
-    //    else
-    //      TimeOut++;
-
-    //    for (int i = 1; i <= 20; i++)
-    //      if (Moyenne.Count > i)
-    //      {
-    //        long point = Moyenne[^i];
-
-    //        if (ping > Max)
-    //          Max = ping;
-
-    //        long maxY = 20 * point / Max;
-
-    //        if (maxY == 0)
-    //          maxY = 1;
-
-    //        for (int y = 1; y <= maxY; y++)
-    //          Pixels.Get(20 - i, 20 - y).SetColor(63 / i, 63 / i, 127 / i);
-    //      }
-
-    //    int x = Moyenne.Average().ToString("0").Length * 4;
-
-    //    Pixels.Set(CaractereList.Print("PING", 1, 1, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
-
-    //    if (showTimeOut)
-    //      Pixels.Set(CaractereList.Print((TimeOut / (double)Moyenne.Count * 100).ToString("0%"), 1, 7, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
-    //    else
-    //      Pixels.Set(CaractereList.Print(Moyenne.Average().ToString("0"), 20 - x, 7, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
-
-    //    x = ping.ToString().Length * 4;
-
-    //    if (reply.Status == IPStatus.Success)
-    //      Pixels.Set(CaractereList.Print(ping.ToString(), 20 - x, 13, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
-    //    else
-    //      Pixels.Set(CaractereList.Print("--", 14, 13, Couleurs.Get("AffDate", "AnneeCouleur", Couleur.Bleu)));
-    //  }
-
-    //  return cycle;
-    //}
   }
 }
